468b661ca9cd1b75593be19e5bc59138d59fe42a
Evren Tükle <tukleevren@gmail.com>
2024-07-18 15:11:26 +0100

    Implements Static Property Details Page

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+
+import ReservationSideBar from "@/app/components/properties/ReservationSideBar";
+
+const PropetyDetailPage = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
+        <Image
+          fill
+          src="/beach_1.jpg"
+          className="object-cover w-full h-full"
+          alt="Beach House"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+        <div className="py-6 pr-6 col-span-3">
+          <h1 className="mb-4 text-4xl">Property Name</h1>
+
+          <span className="mb-6 block text-lg text-gray-600">
+            4 guests | 2 bedrooms | 1 bathroom
+          </span>
+
+          <hr />
+
+          <div className="py-6 flex items-center space-x-4">
+            <Image
+              src="/profile_pic_1.jpg"
+              width={50}
+              height={50}
+              className="rounded-full"
+              alt="The username"
+            />
+
+            <p>
+              <strong>John Doe </strong>
+              is your host
+            </p>
+          </div>
+
+          <hr />
+
+          <div className="mt-6 text-lg">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Necessitatibus fuga, non, asperiores, quaerat ducimus a dolor labore
+            neque nihil optio laborum dignissimos consequuntur fugit.
+            Consequatur distinctio dicta quidem sint beatae?
+          </div>
+        </div>
+
+        <ReservationSideBar />
+      </div>
+    </main>
+  );
+};
+
+export default PropetyDetailPage;
